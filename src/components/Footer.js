df8f3b14df4cf099a7db0d606bec597dad8f2435
Administrator <admin@example.com>
2019-06-09 15:55:08 -0400

    removed empty page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import { Link } from 'gatsby';
+import React from 'react';
 
-import logo from '../assets/images/logo.svg';
 
 const Footer = (props) => (
     <footer id="footer">
