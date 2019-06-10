0aa1be804996b5e9b607a862f483be6d740e61bd
Administrator <admin@example.com>
2019-06-10 14:35:23 -0400

    git error

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,24 @@
 module.exports = {
-  siteMetadata: {
-    title: "Gatsby Starter - Stellar by HTML5 UP",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Stellar by HTML5 UP"
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'CloudPresser.com',
-        short_name: 'CloudPresser',
-        start_url: '/',
-        background_color: '#00d9ff',
-        theme_color: '#00d9ff',
-        display: 'standalone',
-        icon: 'src/assets/images/logo.svg', // This path is relative to the root of the site.
-      },
+    siteMetadata: {
+      title: "Gatsby Starter - Stellar by HTML5 UP",
+      author: "Hunter Chang",
+      description: "A Gatsby.js Starter based on Stellar by HTML5 UP"
     },
-    'gatsby-plugin-sass',
-    'gatsby-plugin-offline'
-  ],
-}
+    plugins: [
+      'gatsby-plugin-react-helmet',
+      {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: 'CloudPresser.com',
+          short_name: 'CloudPresser',
+          start_url: '/',
+          background_color: '#00d9ff',
+          theme_color: '#00d9ff',
+          display: 'standalone',
+          icon: 'src/assets/images/logo.svg', // This path is relative to the root of the site.
+        },
+      },
+      'gatsby-plugin-sass',
+      'gatsby-plugin-offline'
+    ],
+  }
